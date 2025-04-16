418612cbfd02e6b7a8a13337b6fa053b11bb4039
TigerQa <mickelbick@gmail.com>
2025-04-15 20:01:58 -0400

    Add drizzle

---
+++ src/db/schema.ts
@@ -0,0 +1,27 @@
+import { int, integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { relations } from "drizzle-orm";
+
+// Read https://orm.drizzle.team/docs/sql-schema-declaration and https://orm.drizzle.team/docs/rqb for more details
+
+export const users = sqliteTable("users_table", {
+	id: int().primaryKey({ autoIncrement: true }),
+	firstName: text("first_name").notNull(),
+	lastName: int("last_name").notNull(),
+});
+
+export const usersRelations = relations(users, ({ many }) => ({
+	sessions: many(sessions),
+}));
+
+export const sessions = sqliteTable("sessions_table", {
+	id: int().primaryKey({ autoIncrement: true }),
+	userId: int("user_id").notNull(),
+	start: integer({ mode: "timestamp" }).notNull(),
+	end: integer({ mode: "timestamp" }),
+	status: text({ enum: ["ongoing", "finished"] }),
+});
+
+export const sessionsRelations = relations(sessions, ({ one }) => ({
+	user: one(users, { fields: [sessions.userId], references: [users.id] }),
+}));
+
